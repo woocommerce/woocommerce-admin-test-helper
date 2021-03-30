fd5351fe44dd0f8e571b44811396492d2f875e64
Rebecca Scott <me@becdetat.com>
2021-03-30 10:38:42 +1000

    Adding /build directory to release

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => '070d5b3aabc554237ad0382a21edc64b');
