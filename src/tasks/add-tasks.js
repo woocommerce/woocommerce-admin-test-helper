986ee91bb23a8b2018900817c76243fd7e8c8177
Lourens Schep <lourensschep@gmail.com>
2021-04-30 09:30:30 -0300

    Fix formatting

--- src/tasks/add-tasks.js
+++ src/tasks/add-tasks.js
@@ -1,37 +1,38 @@
 import { addFilter } from '@wordpress/hooks';
 
 const TaskComp = () => {
-    return <div>WooCommerce Test Helper task</div>
-}
+	return <div>WooCommerce Test Helper task</div>;
+};
 
 function generateTasks(taskNumber) {
-    const tasks = [];
-    for (let i = 0; i < taskNumber; i++) {
-        tasks.push({
-				key: 'woocommerce_admin_test_helper_'+i,
-				title: 'WooCommerce Test Helper task ' + (i + 1),
-				container: <TaskComp />,
-				completed: Math.floor(Math.random() * 3) === 1,
-				visible: true,
-				additionalInfo: 'Some additional info',
-				time: '5 minutes',
-				isDismissable: Math.floor(Math.random() * 3) === 1,
-			});
-    }
-    return tasks;
+	const tasks = [];
+	for (let i = 0; i < taskNumber; i++) {
+		tasks.push({
+			key: 'woocommerce_admin_test_helper_' + i,
+			title: 'WooCommerce Test Helper task ' + (i + 1),
+			container: <TaskComp />,
+			completed: Math.floor(Math.random() * 3) === 1,
+			visible: true,
+			additionalInfo: 'Some additional info',
+			time: '5 minutes',
+			isDismissable: Math.floor(Math.random() * 3) === 1,
+		});
+	}
+	return tasks;
 }
 
-if (window.wcAdminTestHelperConfig && window.wcAdminTestHelperConfig.taskNumber) {
-    const taskNumber = parseInt(window.wcAdminTestHelperConfig.taskNumber);
-    if (taskNumber > 0) {
-        addFilter(
-            'woocommerce_admin_onboarding_task_list',
-            'woocommerce_admin_test_helper',
-            (tasks) => {
-                return [
-                    ...tasks,
-                    ...generateTasks(taskNumber)
-                ]
-            });
-    }
+if (
+	window.wcAdminTestHelperConfig &&
+	window.wcAdminTestHelperConfig.taskNumber
+) {
+	const taskNumber = parseInt(window.wcAdminTestHelperConfig.taskNumber);
+	if (taskNumber > 0) {
+		addFilter(
+			'woocommerce_admin_onboarding_task_list',
+			'woocommerce_admin_test_helper',
+			(tasks) => {
+				return [...tasks, ...generateTasks(taskNumber)];
+			}
+		);
+	}
 }
