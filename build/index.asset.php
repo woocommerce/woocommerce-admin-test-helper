863654f8e8f9806bc69ed3d9ff062e89efa9435e
Moon <moon.kyong@automattic.com>
2022-02-28 12:20:44 -0800

    Adding /build directory to release

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-data-controls', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => 'ba3f7ae5034159ce4eaa702a7bbce2ef');
